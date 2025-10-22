f6874e2cfbe5e4affe518e3f1231aebfe8bd9e95
dean <jerekitibwa@gmail.com>
2025-10-22 10:34:00 +0300

    meaning.java and words.java have been added
    the files meaning.java and words.java complement the code expansion in Main.java.

---
+++ meaning.java
@@ -0,0 +1,15 @@
+package project;
+
+public class meaning {
+    String Defination;
+    String Notes;
+
+    public meaning(String Defination, String Notes) {
+        this.Defination = Defination;
+        this.Notes = Notes;
+    }
+
+    public void describe() {
+        System.out.println("Defination: " + Defination + ", Notes: " + Notes);
+    }
+}
---
+++ words.java
@@ -0,0 +1,21 @@
+package project;
+
+public class words extends meaning{
+    String Polymorphism;
+    String Inheritance;
+
+    public words(String Defination, String Notes, String Polymorphism, String Inheritance) {
+        super(Defination, Notes);
+        this.Polymorphism = Polymorphism;
+        this.Inheritance = Inheritance;
+    }
+
+    @Override
+    public void describe() {
+        System.out.println("Defination: " + Defination + ", Notes: " + Notes + ", Polymorphism: " + Polymorphism + ", Inheritance: " + Inheritance);
+    }
+
+    public void explain() {
+        System.out.println("This is the explain method in words class.");
+    }
+}
